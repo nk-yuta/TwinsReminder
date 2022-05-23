5c41a9b23e1b85e5f6c767be3ce5508af532e758
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-05-23 17:57:24 +0900

    マイページの作成

--- TwinsReminder/account/urls.py
+++ TwinsReminder/account/urls.py
@@ -7,6 +7,7 @@ app_name ='account'
 
 urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
-    path('login/', views.Login.as_view(), name='login'),
-    path('logout/', views.Logout.as_view(), name='logout'),
+    path('login/', views.Login.as_view(), name='login'), # ログイン
+    path('logout/', views.Logout.as_view(), name='logout'), # ログアウト
+    path('my_page/<int:pk>/', views.MyPage.as_view(), name='my_page'), # マイページ
 ]
--- TwinsReminder/account/views.py
+++ TwinsReminder/account/views.py
@@ -1,7 +1,10 @@
-from .forms import LoginForm
+from django.shortcuts import redirect # 追加
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import get_user_model # 追加
+from django.contrib.auth.mixins import UserPassesTestMixin # 追加
 from django.views import generic
+
 
 #トップページ
 class TopView(generic.TemplateView):
@@ -15,3 +18,20 @@ class Login(LoginView):
 # ログアウト
 class Logout(LogoutView):
     template_name = 'account/logout_done.html'
+
+# 自分しかアクセスできないようにするMixin(My Pageのため)
+class OnlyYouMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        # 今ログインしてるユーザーのpkと、そのマイページのpkが同じなら許可
+        user = self.request.user
+        return user.pk == self.kwargs['pk']
+
+# マイページ
+class MyPage(OnlyYouMixin, generic.DetailView):
+    # ユーザーモデルの取得
+    User = get_user_model()
+    model = User
+    template_name = 'account/my_page.html'
+    # モデル名小文字(user)でモデルインスタンスがテンプレートファイルに渡される
