3d535a4b2ef3337e749962d39ccae7748e077bda
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-05-23 15:57:39 +0900

    アプリの登録とテンプレの場所の設定

--- TwinsReminder/TwinsReminder/settings.py
+++ TwinsReminder/TwinsReminder/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig', # ユーザー認証
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'TwinsReminder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # 直下のtemplatesを指定
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
