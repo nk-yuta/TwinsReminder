bdbe0eceb2bf2e54a0fc60ffa882341ecf31b363
rin-m <s2010048@klis.tsukuba.ac.jp>
2022-05-30 16:13:29 +0900

    urls.pyの修正

--- TwinsReminder/TwinsReminder/urls.py
+++ TwinsReminder/TwinsReminder/urls.py
@@ -17,13 +17,10 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include # 追加
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('account.urls')),  # 追加
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('account.urls')),  # 追加
     path('show/', include('show.urls')),
 ]
