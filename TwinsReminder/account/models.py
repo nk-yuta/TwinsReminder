1761169f841c6bb5341730db079c9d199f83ab98
NakamuraYuta-klis <s1910702@klis.tsukuba.ac.jp>
2022-06-13 17:07:12 +0900

    通知一覧の表示機能を追加

--- TwinsReminder/account/models.py
+++ TwinsReminder/account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+import os
 
-# Create your models here.
+def file_path():
+    return os.path.join(settings.LOCAL_FILE_DIR, "attached_files")
+
+class User_Notification(models.Model):
+    category = models.CharField(max_length=50)
+    date = models.DateTimeField('date published')
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    #file = models.FilePathField("/files")
+    instructor = models.CharField(max_length=50)
+    student = models.CharField(max_length=50)
